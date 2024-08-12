766bd4795757718fd3d614392721ca651498a149
Manvel <Manvelnavoan@gmail.com>
2024-08-12 20:26:09 +0300

    first commit task3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        int i = 5;
+
+        while (i <= 70)
+        {
+            sum += i;
+            i++;
+        }
+        
+        Console.WriteLine("Сумма чисел от 5 до 70: " + sum);
+    }
+}
